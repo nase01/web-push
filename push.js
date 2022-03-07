f07bc851b9aff49dd5e1664bfad5a9f1931f39af
nase01 <naessony01@gmail.com>
2022-03-08 00:18:29 +0800

    Fix

--- push.js
+++ push.js
@@ -9,6 +9,13 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@connected.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub = {}
+let sub = {
+    "endpoint":"https://fcm.googleapis.com/fcm/send/fQpR6H9ER2Q:APA91bGQX3-p7aBa0_xX1B5LuFxUWWVhBf06rHG-C1z3UYhy3hTpbsAazOfQYM48zUMJTBjBE9N4eQ_WGqWMF9Edwc0ztbOyO7bk7tLwAkQeTGYnjjUIYQIvnoepVQ9Yr1LvE97dBHmx",
+    "expirationTime":null,
+    "keys":{
+        "p256dh":"BII04i-Wj6RpgWoiZans32ZXzPWFcoTb6aAYHkDCIn_8QwGoMUrOlqQnQuHbzgOvt2XoGbsRbuzPe80nYtOIODQ",
+        "auth":"Rx2tfEM6-IFEomevNcdDDA"
+    }
+};
 
 push.sendNotification(sub, 'test message');
--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
 self.addEventListener('push', () => {
-    self.registration.sendNofitication('test message', {})
-})
+    var options = {
+        body: 'This notification was generated from a push!',
+        icon: 'images/example.pnh',
+        vibrate: [100,50,100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            { 
+                action: 'explore', 
+                title: 'Explore this new world',
+                icon: 'images/checkmark.png'
+            },
+            {
+                action: 'close',
+                title: 'Close',
+                icon: 'images/xmark.png'
+            }
+        ]
+    };
+    e.waitUntil(
+        self.registration.sendNofitication('test message', options)
+    );
+});
